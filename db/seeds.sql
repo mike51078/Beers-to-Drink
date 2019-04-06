26866032d8e60132b51553aaf48762396b290d39
elcid00 <elcid00@aol.com>
2019-04-06 13:21:21 -0400

    more setup done

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (name) VALUES ('Breakfast Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Iron City Burger', true);
