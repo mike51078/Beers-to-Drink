65e1a35370fbc7b979249c9078cede1b358202df
elcid00 <elcid00@aol.com>
2019-04-06 15:43:40 -0400

    dbeaver updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO burgers (name) VALUES ('Breakfast Burger');
-INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Iron City Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Breakfast Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Iron City Burger', true);
