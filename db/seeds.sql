939d779d2c5ec3a434b7b927e114510fcb590b70
elcid00 <elcid00@aol.com>
2019-04-09 09:59:24 -0400

    changed db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
 
-INSERT INTO burgers (burger_name) VALUES ('Breakfast Burger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Iron City Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Jai Lai');
+INSERT INTO burgers (burger_name) VALUES ('High Stepper');
+INSERT INTO burgers (burger_name, devoured) VALUES ("Devil's Chair", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Bags", true);
+INSERT INTO burgers (burger_name) VALUES ('OBP');
+INSERT INTO burgers (burger_name) VALUES ('I4 IPA');
